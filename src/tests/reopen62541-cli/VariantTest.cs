5198480935e2963b1fd8872474d678c4ad384347
Juergen Hock <juergen.hock@jurihock.de>
2021-04-30 22:16:48 +0200

    Create scalar variant test

---
+++ src/tests/reopen62541-cli/VariantTest.cs
@@ -0,0 +1,63 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+[TestClass]
+public class VariantTest
+{
+  static readonly string[] Scalars = { "Scalars" };
+
+  [TestMethod]
+  public void ScalarTest()
+  {
+    using (var server = new UA.Server())
+    {
+      server.AddObject(Scalars.First(), Scalars.First());
+
+      server.AddVariable<bool>(nameof(Boolean), nameof(Boolean), Scalars, () => true);
+
+      server.AddVariable<sbyte>(nameof(SByte), nameof(SByte), Scalars, () => -1);
+      server.AddVariable<byte>(nameof(Byte), nameof(Byte), Scalars, () => +1);
+
+      server.AddVariable<short>(nameof(Int16), nameof(Int16), Scalars, () => -1);
+      server.AddVariable<ushort>(nameof(UInt16), nameof(UInt16), Scalars, () => +1);
+
+      server.AddVariable<int>(nameof(Int32), nameof(Int32), Scalars, () => -1);
+      server.AddVariable<uint>(nameof(UInt32), nameof(UInt32), Scalars, () => +1U);
+
+      server.AddVariable<long>(nameof(Int64), nameof(Int64), Scalars, () => -1L);
+      server.AddVariable<ulong>(nameof(UInt64), nameof(UInt64), Scalars, () => +1UL);
+
+      server.AddVariable<float>(nameof(Single), nameof(Single), Scalars, () => -1.0f);
+      server.AddVariable<double>(nameof(Double), nameof(Double), Scalars, () => +1.0d);
+
+      server.AddVariable<string>(nameof(String), nameof(String), Scalars, () => "string");
+
+      server.RunAsync();
+
+      using (var client = new UA.Client())
+      {
+        client.Connect();
+
+        Assert.IsTrue(client.Get<bool>(nameof(Boolean), Scalars));
+
+        Assert.AreEqual(-1, client.Get<sbyte>(nameof(SByte), Scalars));
+        Assert.AreEqual(+1, client.Get<byte>(nameof(Byte), Scalars));
+
+        Assert.AreEqual(-1, client.Get<short>(nameof(Int16), Scalars));
+        Assert.AreEqual(+1, client.Get<ushort>(nameof(UInt16), Scalars));
+
+        Assert.AreEqual(-1, client.Get<int>(nameof(Int32), Scalars));
+        Assert.AreEqual(+1U, client.Get<uint>(nameof(UInt32), Scalars));
+
+        Assert.AreEqual(-1L, client.Get<long>(nameof(Int64), Scalars));
+        Assert.AreEqual(+1UL, client.Get<ulong>(nameof(UInt64), Scalars));
+
+        Assert.AreEqual(-1.0f, client.Get<float>(nameof(Single), Scalars));
+        Assert.AreEqual(+1.0d, client.Get<double>(nameof(Double), Scalars));
+
+        Assert.AreEqual("string", client.Get<string>(nameof(String), Scalars));
+      }
+    }
+  }
+}
